9c114112e9fec703ad39d8c1d731fd67619e1fe6
janic <git@janic.io>
2023-03-31 11:47:24 +0200

    feat(streaming-server): Implemented HLS streaming

--- client/src/components/Livestream.tsx
+++ client/src/components/Livestream.tsx
@@ -1,15 +1,36 @@
-import React from "react";
-import ReactPlayer from "react-player";
+import React, { MutableRefObject, useRef, useState } from "react";
+import ReactHlsPlayer from "react-hls-player";
+import mutedIcon from "../../public/icons/volume-xmark.svg";
 
 function Livestream() {
+  const playerRef =
+    useRef<HTMLVideoElement>() as MutableRefObject<HTMLVideoElement>;
+
+  const [isMuted, setIsuted] = useState(true);
+
   return (
-    <div className="aspect-video bg-red-700">
-      <ReactPlayer
-        url="https://www.youtube.com/watch?v=adKx8AITK44"
-        height="100%"
-        width="100%"
-      />
-    </div>
+    <>
+      <div className="relative aspect-video bg-red-700">
+        <ReactHlsPlayer
+          playerRef={playerRef}
+          muted={true}
+          autoPlay={true}
+          src="https://live.uhc-elgg.ch/stream"
+          width="100%"
+          height="auto"
+        />
+        <button
+          onClick={() => {
+            playerRef.current.muted = false;
+            setIsuted(false);
+          }}
+          style={{ opacity: isMuted ? 1 : 0 }}
+          className="absolute left-8 top-8 cursor-pointer rounded bg-white p-4 transition-opacity duration-100"
+        >
+          <img className="h-8 w-8" src={mutedIcon} />
+        </button>
+      </div>
+    </>
   );
 }
 
