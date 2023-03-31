6cf700249ad8d73e23c0de00b5d69e9c34093858
janic <git@janic.io>
2023-04-01 00:02:17 +0200

    fix(game-in-ten-minutes): Fixed update in ten minutes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,47 +1,67 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 dotenv.config();
 
-import cors from "@fastify/cors";
-import path from "path";
-import fstatic from "@fastify/static";
-import fastify from "fastify";
-import { fastifyTRPCPlugin } from "@trpc/server/adapters/fastify";
-import ws from "@fastify/websocket";
-import { appRouter } from "./trpc";
-import adminRoutes from "./admin";
-import { createContext } from "./context";
+import cors from '@fastify/cors';
+import path from 'path';
+import fstatic from '@fastify/static';
+import fastify from 'fastify';
+import { fastifyTRPCPlugin } from '@trpc/server/adapters/fastify';
+import ws from '@fastify/websocket';
+import {
+	accountCount,
+	appRouter,
+	commentCount,
+	totalViewCount,
+	viewCount,
+} from './trpc';
+import adminRoutes from './admin';
+import { createContext } from './context';
 
 const server = fastify();
 
-server.get("/ping", async (_, res) => {
-    res.send("pong");
+server.get('/ping', async (_, res) => {
+	res.send('pong');
 });
 
 server.register(fstatic, {
-    root: path.join(__dirname, "..", "..", "client", "dist"),
-    prefix: "/",
+	root: path.join(__dirname, '..', '..', 'client', 'dist'),
+	prefix: '/',
 });
 
 server.register(ws);
 server.register(fastifyTRPCPlugin, {
-    prefix: "/trpc",
-    useWSS: true,
-    useWss: true,
-    trpcOptions: { router: appRouter, createContext },
+	prefix: '/trpc',
+	useWSS: true,
+	useWss: true,
+	trpcOptions: { router: appRouter, createContext },
 });
 
 server.register(cors, {
-    origin: true,
+	origin: true,
+});
+
+server.get('/metrics', (_, res) => {
+	const data = {
+		uhc_elgg_live_total_viewer_count: totalViewCount,
+		uhc_elgg_live_total_account_count: accountCount,
+		uhc_elgg_live_total_comment_count: commentCount,
+		uhc_elgg_live_current_view_count: viewCount,
+	};
+	res.send(
+		Object.entries(data)
+			.map((key, value) => key + '\t' + value.toString())
+			.join('\n')
+	);
 });
 
 adminRoutes(server);
 
 (async () => {
-    try {
-        await server.listen({ port: 3000, host: "0.0.0.0" });
-        console.log(`Server listening on 3000`);
-    } catch (err) {
-        server.log.error(err);
-        process.exit(1);
-    }
+	try {
+		await server.listen({ port: 3000, host: '0.0.0.0' });
+		console.log(`Server listening on 3000`);
+	} catch (err) {
+		server.log.error(err);
+		process.exit(1);
+	}
 })();
